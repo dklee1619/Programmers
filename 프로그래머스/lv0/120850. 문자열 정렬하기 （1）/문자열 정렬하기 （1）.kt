e97d26f43fd486318cf3939ad31d349b52249ee0
dklee1619 <138475088+dklee1619@users.noreply.github.com>
2023-09-02 08:42:04 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 11.20 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.kt
@@ -0,0 +1,45 @@
+class Solution {
+    fun solution(my_string: String): IntArray {
+        var answer: IntArray = intArrayOf()
+        var s = my_string.toCharArray()
+        var size1 = s.size
+        var array: IntArray = intArrayOf()
+        for (i in 0..size1 - 1) {
+            for (j in 0..9) {
+                if (s[i] == (j + '0'.toInt()).toChar()) {
+                    array = array.plus(j)
+                    break
+                }
+            }
+        }
+        var size2 = array.size
+        var state = 0
+        for (k in 0..9) {
+            state = 0
+            while (state == 0) {
+                for (l in 0..size2 - 1) {
+                    if (k == array[l]) {
+                        answer = answer.plus(k)
+                    }
+                    if (l == size2 - 1) {
+                        state = 1
+                    }
+                }
+            }
+        }
+
+        return answer
+    }
+}
+// 1. char배열로
+// 2. for 0..size-1 까지 하고
+// 3. for 0..9까지 해서 '0'~'9'까지 있는지 확인
+// 4. 있으면 새로운 배열에 담고
+// 5. 오름차순을 하기 위한 방법 생각하기
+// 6. 일단 intarray로 바꿔주고 => 그냥 인덱스를 담음
+// 7. for i in 0..size2-1 해주고
+// 8. for j in 0..9 해주고
+// 9. 발견되는거 i로 바꿔주고, break  
+// 10. 코드짜기 시작 !
+// 11. 문제점 발생. 7~8에서 0~9에대해 한번밖에 검사를 못함.
+// 12. while을 통해 찾아야만 넘어갈수 잇게끔 해보자.
